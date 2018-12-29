10856b28195c0e6d052da137b9809e154923d974
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2018-12-29 16:05:31 +0100

    Ending directories renamming

--- basic-vanilla-custom-element/basic-vanilla-custom-element.js
+++
@@ -1,5 +0,0 @@
-customElements.define('basic-vanilla-custom-element', class extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = "<b>I'm an basic custom element defined in vanilla JS!</b>";
-  }
-});
